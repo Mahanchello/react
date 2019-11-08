8d7e0957b76519fa323ef0dca20f76ed6fffe8b9
Anastasiya <amakhankova@shutterstock.com>
2019-11-07 19:59:32 -0500

    Some cleanup

--- my-react-app/src/userInput/UserInput.js
+++ my-react-app/src/userInput/UserInput.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const userInput = (props) => (
   <div className="Input">
-    <input type="text" onChange={props.changed} value={props.library} value={props.currentLibrary} />
+    <input type="text"
+      onChange={props.changed}
+      value={props.library}
+      value={props.currentLibrary} />
   </div>
 );
 
